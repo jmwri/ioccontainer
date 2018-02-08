9ddee075f230dfd643c1e0c13ada4da390c02d47
Jim Wright <jim@linx.net>
2018-02-08 20:06:08 +0000

    Change py-container to pycontainer

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from distutils.core import setup
 setup(
-  name='py-container',
-  packages=['py-container'],  # this must be the same as the name above
+  name='pycontainer',
+  packages=['pycontainer'],  # this must be the same as the name above
   version='0.1',
   description='Service container for automatic dependency injection',
   author='Jim Wright',
   author_email='jmwri93@gmail.com',
-  url='https://github.com/jmwri/py-container',
-  download_url='https://github.com/jmwri/py-container/archive/0.1.tar.gz',
+  url='https://github.com/jmwri/pycontainer',
+  download_url='https://github.com/jmwri/pycontainer/archive/0.1.tar.gz',
   keywords=['ioc', 'di', 'dependency', 'injection', 'container'],
   classifiers=[],
 )
