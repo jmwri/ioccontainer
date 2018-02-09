080dbaea4fc5bd21ec13ca1ebff6a5aabe8d2f00
Jim Wright <jim@linx.net>
2018-02-09 01:02:06 +0000

    Update setup

--- setup.py
+++ setup.py
@@ -4,6 +4,8 @@ setup(
     name='ioccontainer',
     packages=['ioccontainer'],  # this must be the same as the name above
     version='1.0.0',
+    license='MIT',
+    python_requires='>=3.6, <4',
     description='Service container for automatic dependency injection',
     author='Jim Wright',
     author_email='jmwri93@gmail.com',
@@ -16,16 +18,9 @@ setup(
         #   4 - Beta
         #   5 - Production/Stable
         'Development Status :: 3 - Alpha',
-
-        # Indicate who your project is intended for
         'Intended Audience :: Developers',
-        'Topic :: Software Development :: Inversion Of Control',
-
-        # Pick your license as you wish (should match "license" above)
+        'Topic :: Software Development',
         'License :: OSI Approved :: MIT License',
-
-        # Specify the Python versions you support here. In particular, ensure
-        # that you indicate whether you support Python 2, Python 3 or both.
         'Programming Language :: Python :: 3.6',
     ],
 )
