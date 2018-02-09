e41dafbacf40796bd4c956dc7c37eec26ec47219
Jim Wright <jim@linx.net>
2018-02-09 00:51:06 +0000

    Increment version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name='ioccontainer',
     packages=['ioccontainer'],  # this must be the same as the name above
-    version='0.0.1',
+    version='1.0.0',
     description='Service container for automatic dependency injection',
     author='Jim Wright',
     author_email='jmwri93@gmail.com',
