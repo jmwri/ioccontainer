3f36af5580fa44027acba55992f49cf24741f9c8
Jim Wright <jim@linx.net>
2018-02-09 00:50:48 +0000

    Fix indentation in setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,31 @@
 from distutils.core import setup
+
 setup(
-  name='ioccontainer',
-  packages=['ioccontainer'],  # this must be the same as the name above
-  version='0.0.1',
-  description='Service container for automatic dependency injection',
-  author='Jim Wright',
-  author_email='jmwri93@gmail.com',
-  url='https://github.com/jmwri/ioccontainer',
-  download_url='https://github.com/jmwri/ioccontainer/archive/0.1.tar.gz',
-  keywords=['ioc', 'di', 'dependency', 'injection', 'container'],
-  classifiers=[],
+    name='ioccontainer',
+    packages=['ioccontainer'],  # this must be the same as the name above
+    version='0.0.1',
+    description='Service container for automatic dependency injection',
+    author='Jim Wright',
+    author_email='jmwri93@gmail.com',
+    url='https://github.com/jmwri/ioccontainer',
+    download_url='https://github.com/jmwri/ioccontainer/archive/0.1.tar.gz',
+    keywords=['ioc', 'di', 'dependency', 'injection', 'container'],
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 3 - Alpha',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Inversion Of Control',
+
+        # Pick your license as you wish (should match "license" above)
+        'License :: OSI Approved :: MIT License',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 3.6',
+    ],
 )
